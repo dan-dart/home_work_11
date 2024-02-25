fae5f3952d452549a451a539fea78d57b382d99d
Eshmatov Daniir <eshdan0@gmail.com>
2024-02-25 21:37:49 +0600

    1st

---
+++ test/lesson_with_circles_4_test.dart
@@ -0,0 +1,8 @@
+import 'package:lesson_with_circles_4/lesson_with_circles_4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
